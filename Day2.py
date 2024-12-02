eaea4747199677048c79f3a43189dbc460316ee8
MartinXu <20XuPM@tonbridge-school.org>
2024-12-02 22:00:41 +0000

    start

--- Day2.py
+++ Day2.py
@@ -1,2 +1,32 @@
 Input = [i.strip("\n").split() for i in open("input.txt", "r")]
-print(Input)
+total = 0
+
+
+def check(lst):
+    if int(lst[0]) > int(lst[1]):
+        lst = lst[::-1]
+    for element in range(1, len(lst)):
+        if 1 <= int(lst[element]) - int(lst[element - 1]) <= 3:
+            continue
+        else:
+            return False
+    return True
+
+
+for line in Input:
+    if int(line[0]) > int(line[1]):
+        line = line[::-1]
+    for element in range(1, len(line)):
+        if 1 <= int(line[element]) - int(line[element - 1]) <= 3:
+            continue
+        else:
+            for i in range(len(line)):
+                current = list(line)
+                current.pop(i)
+                if check(current):
+                    break
+            else:
+                break
+    else:
+        total += 1
+print(total)
